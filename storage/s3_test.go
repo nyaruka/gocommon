bb64ad545eb718a0fb181adb771469a95aa3ec76
Rowan Seymour <rowanseymour@gmail.com>
2022-10-31 12:43:16 -0500

    Fix passing ACL to S3 puts

--- storage/s3_test.go
+++ storage/s3_test.go
@@ -15,6 +15,10 @@ import (
 )
 
 type testS3Client struct {
+	headBucketInputs []*s3.HeadBucketInput
+	getObjectInputs  []*s3.GetObjectInput
+	putObjectInputs  []*s3.PutObjectInput
+
 	returnError           error
 	headBucketReturnValue *s3.HeadBucketOutput
 	getObjectReturnValue  *s3.GetObjectOutput
@@ -22,18 +26,24 @@ type testS3Client struct {
 }
 
 func (c *testS3Client) HeadBucketWithContext(ctx context.Context, input *s3.HeadBucketInput, opts ...request.Option) (*s3.HeadBucketOutput, error) {
+	c.headBucketInputs = append(c.headBucketInputs, input)
+
 	if c.returnError != nil {
 		return nil, c.returnError
 	}
 	return c.headBucketReturnValue, nil
 }
 func (c *testS3Client) GetObjectWithContext(ctx context.Context, input *s3.GetObjectInput, opts ...request.Option) (*s3.GetObjectOutput, error) {
+	c.getObjectInputs = append(c.getObjectInputs, input)
+
 	if c.returnError != nil {
 		return nil, c.returnError
 	}
 	return c.getObjectReturnValue, nil
 }
 func (c *testS3Client) PutObjectWithContext(ctx context.Context, input *s3.PutObjectInput, opts ...request.Option) (*s3.PutObjectOutput, error) {
+	c.putObjectInputs = append(c.putObjectInputs, input)
+
 	if c.returnError != nil {
 		return nil, c.returnError
 	}
@@ -66,6 +76,10 @@ func TestS3Get(t *testing.T) {
 	assert.Equal(t, "text/plain", contentType)
 	assert.Equal(t, []byte(`HELLOWORLD`), contents)
 
+	assert.Len(t, client.getObjectInputs, 1)
+	assert.Equal(t, aws.String("mybucket"), client.getObjectInputs[0].Bucket)
+	assert.Equal(t, aws.String("/foo/things"), client.getObjectInputs[0].Key)
+
 	client.returnError = errors.New("boom")
 
 	_, _, err = s.Get(ctx, "/foo/things")
@@ -80,6 +94,11 @@ func TestS3Put(t *testing.T) {
 	url, err := s.Put(ctx, "/foo/things", "text/plain", []byte(`HELLOWORLD`))
 	assert.NoError(t, err)
 	assert.Equal(t, "https://mybucket.s3.us-east-1.amazonaws.com/foo/things", url)
+
+	assert.Len(t, client.putObjectInputs, 1)
+	assert.Equal(t, aws.String("mybucket"), client.putObjectInputs[0].Bucket)
+	assert.Equal(t, aws.String("/foo/things"), client.putObjectInputs[0].Key)
+	assert.Equal(t, aws.String(s3.BucketCannedACLPublicRead), client.putObjectInputs[0].ACL)
 
 	client.returnError = errors.New("boom")
 
