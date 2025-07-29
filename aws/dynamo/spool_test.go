2e843e1061423e728957c714aef16a0a62475cd1
Rowan Seymour <rowanseymour@gmail.com>
2025-07-29 16:00:16 -0500

    Use different tables for different tests in dynamo package

--- aws/dynamo/spool_test.go
+++ aws/dynamo/spool_test.go
@@ -21,9 +21,9 @@ func TestSpool(t *testing.T) {
 	client, err := dynamo.NewClient("root", "tembatemba", "us-east-1", "http://localhost:6000")
 	assert.NoError(t, err)
 
-	defer dynamo.Drop(ctx, client, "TestThings")
+	defer dynamo.Drop(ctx, client, "TestSpool")
 
-	createTestTable(t, client, "TestThings")
+	createTestTable(t, client, "TestSpool")
 
 	item1, _ := dynamo.Marshal(&ThingItem{ThingKey: ThingKey{PK: "P11", SK: "SAA"}, Name: "Thing 1", Count: 123})
 	item2, _ := dynamo.Marshal(&ThingItem{ThingKey: ThingKey{PK: "P22", SK: "SBB"}, Name: "Thing 2", Count: 234})
@@ -37,14 +37,14 @@ func TestSpool(t *testing.T) {
 	err = spool.Start()
 	assert.NoError(t, err)
 
-	err = spool.Add("TestThings", []map[string]types.AttributeValue{item1, item2})
+	err = spool.Add("TestSpool", []map[string]types.AttributeValue{item1, item2})
 	assert.NoError(t, err)
-	err = spool.Add("TestThings", []map[string]types.AttributeValue{item3})
+	err = spool.Add("TestSpool", []map[string]types.AttributeValue{item3})
 	assert.NoError(t, err)
 
 	assert.Equal(t, 3, spool.Size())
-	assert.FileExists(t, "./_test_spool/01984174-5600-7000-aded-7d8b151cbd5b#2@TestThings.jsonl")
-	assert.FileExists(t, "./_test_spool/01984174-59e8-7000-b664-880fc7581c77#1@TestThings.jsonl")
+	assert.FileExists(t, "./_test_spool/01984174-5600-7000-aded-7d8b151cbd5b#2@TestSpool.jsonl")
+	assert.FileExists(t, "./_test_spool/01984174-59e8-7000-b664-880fc7581c77#1@TestSpool.jsonl")
 
 	spool.Stop()
 	wg.Wait()
@@ -59,7 +59,7 @@ func TestSpool(t *testing.T) {
 
 	assert.Equal(t, 0, spool.Size())
 
-	obj, err := dynamo.GetItem[ThingKey, ThingItem](ctx, client, "TestThings", ThingKey{PK: "P11", SK: "SAA"})
+	obj, err := dynamo.GetItem[ThingKey, ThingItem](ctx, client, "TestSpool", ThingKey{PK: "P11", SK: "SAA"})
 	assert.NoError(t, err)
 	assert.Equal(t, "Thing 1", obj.Name)
 
